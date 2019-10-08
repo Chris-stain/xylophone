f6d607e1880155dbbedb8c1a65790b7c004393be
Christian Jimenez <Chris_Stain@Christians-Mini.home>
2019-10-08 14:50:48 -0700

    Initial Commit

---
+++ Xylophone/ VIewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Xylophone
+//
+//  Created by Christian Jimenez on 10/08/2019.
+//  Copyright © 2019 Oavi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController{
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+
+    @IBAction func notePressed(_ sender: UIButton) {
+        
+        print(sender.tag)
+        
+        
+    }
+    
+  
+
+}
+
